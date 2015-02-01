f0a7d6bdb4c2bafad5fc81adb6fd914f840688d7
Julien <julien.mageau@live.ca>
2015-02-01 16:22:20 -0500

    Re-enabled blending, removed blending from individual shapes' draw

--- src/net/wintastic/Game.java
+++ src/net/wintastic/Game.java
@@ -67,8 +67,8 @@ public class Game implements LWJGLGame {
 
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-//        GL11.glEnable(GL11.GL_BLEND);
-//        GL11.glBlendFunc(GL11.GL_DST_ALPHA, GL11.GL_ONE_MINUS_DST_ALPHA);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     public static void setDisplayMode(int width, int height, boolean fullscreen) {
