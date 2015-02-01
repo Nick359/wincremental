5a7159a46e8a66d75591224dea43566001767443
Julien <julien.mageau@live.ca>
2015-01-31 23:15:23 -0500

    Re-enabled some GL stuff

--- src/net/wintastic/Game.java
+++ src/net/wintastic/Game.java
@@ -65,10 +65,10 @@ public class Game implements LWJGLGame {
 
         GL11.glEnable(GL11.GL_POINT_SMOOTH);
 
-//        GL11.glEnable(GL11.GL_TEXTURE_2D);
-//        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-//        GL11.glEnable(GL11.GL_BLEND);
-//        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     public static void setDisplayMode(int width, int height, boolean fullscreen) {
