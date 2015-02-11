00167d7cd5c5495046f8e6c0b7ae7fa02fcab75a
Julien <julien.mageau@live.ca>
2015-02-10 20:13:59 -0500

    Changed Tile System to use TileType instead of creating Tiles of different types
    Temporarily disabled TileRadiusIndicator

--- src/net/wintastic/wincremental/tiles/Board.java
+++ src/net/wintastic/wincremental/tiles/Board.java
@@ -1,32 +1,41 @@
 package net.wintastic.wincremental.tiles;
 
+import net.wintastic.lwjgl.DrawBatch;
+import net.wintastic.lwjgl.Drawable;
 import net.wintastic.lwjgl.Input;
 import net.wintastic.lwjgl.Pair;
 import net.wintastic.util.math.MathHelper;
 import net.wintastic.wincremental.GameManager;
 import net.wintastic.wincremental.gui.MenuBar;
+import net.wintastic.wincremental.tiles.Tile.TileType;
 
-public class Board {
-    private Tile[][] tiles;
+public class Board implements Drawable {
+    private TileType[][] tiles;
     private final int width, height;
-    private static Tile selectedTile;
+    private Pair<Integer> selectedTilePosition;
+    private boolean visible;
+    private float layerDepth;
 
     public Board() {
         this.width = GameManager.mapWidth;
         this.height = GameManager.mapHeight;
         initializeTiles();
+        this.visible = true;
+        this.layerDepth = 0.2f;
+
+        DrawBatch.add(this);
     }
 
     private void initializeTiles() {
-        tiles = new Tile[width][height];
+        tiles = new TileType[width][height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 if (MathHelper.randomChance(0.2f))
-                    tiles[i][j] = new ResourceTile(new Pair<Integer>(i, j), ResourceTile.ResourceTileType.GOLD);
+                    tiles[i][j] = TileType.GOLD;
                 else if (MathHelper.randomChance(4))
-                    tiles[i][j] = new ResourceTile(new Pair<Integer>(i, j), ResourceTile.ResourceTileType.WOOD);
+                    tiles[i][j] = TileType.WOOD;
                 else
-                    tiles[i][j] = new ResourceTile(new Pair<Integer>(i, j), ResourceTile.ResourceTileType.GRASS);
+                    tiles[i][j] = TileType.EMPTY;
             }
         }
 
@@ -34,38 +43,73 @@ public class Board {
     }
 
     private void placeTownCenter() {
-        setTile(new Pair<Integer>(width / 2, height / 2), new BuildingTile(new Pair<Integer>(width / 2, height / 2), BuildingTile.BuildingTileType.TOWN_CENTER));
+        setTile(new Pair<Integer>(width / 2, height / 2), TileType.TOWN_CENTER);
     }
 
-    public Tile getTile(Pair<Integer> position) {
+    public TileType getTile(Pair<Integer> position) {
         return tiles[position.first][position.second];
     }
 
-    public void setTile(Pair<Integer> position, Tile newTile) {
+    public void setTile(Pair<Integer> position, TileType newTile) {
         tiles[position.first][position.second] = newTile;
     }
 
-    public static void setSelectedTile(Tile selectedTile) {
-        Board.selectedTile = selectedTile;
+    public void setSelectedTile(Pair<Integer> position) {
+        this.selectedTilePosition = position;
     }
 
     public void update() {
         if (mouseInBoard() && Input.isButtonPressed(0)) {
-            if (selectedTile != null) {
-                ((BuildingTile) selectedTile).toggleSelected();
-            }
+//            if (selectedTilePosition != null) {
+//                ((BuildingTile) selectedTile).toggleSelected();
+//            }
 
             Pair<Integer> p = new Pair<Integer>((int) ((Input.mousePosition().x - GameManager.menuBarWidth + GameManager.camera.getPosition().x) / GameManager.tileSize),
                     (int) ((Input.mousePosition().y - GameManager.toolbarHeight + GameManager.camera.getPosition().y) / GameManager.tileSize));
-            Tile t = tiles[p.first][p.second];
-            t.clickAction();
-            if (t instanceof ResourceTile && ((ResourceTile) t).type == ResourceTile.ResourceTileType.GRASS && MenuBar.selectedIcon != null && MenuBar.selectedIcon.selected) {
-                setTile(p, new BuildingTile(t.position, MenuBar.selectedIcon.type.getBuildingTileType()));
+            TileType type = tiles[p.first][p.second];
+            type.clickAction();
+            if (type == TileType.EMPTY && MenuBar.selectedIcon != null && MenuBar.selectedIcon.selected) {
+                setTile(p, MenuBar.selectedIcon.type.getBuildingTileType());
             }
         }
     }
 
     public boolean mouseInBoard() {
         return Input.mousePosition().x >= GameManager.menuBarWidth && Input.mousePosition().x < GameManager.resX && Input.mousePosition().y >= GameManager.toolbarHeight && Input.mousePosition().y < GameManager.resY;
+    }
+
+    @Override
+    public void setLayerDepth(float layerDepth) {
+        this.layerDepth = layerDepth;
+    }
+
+    @Override
+    public float getLayerDepth() {
+        return this.layerDepth;
+    }
+
+    @Override
+    public void draw() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Pair<Integer> p = new Pair<Integer>(i, j);
+                if (Tile.isVisible(p))
+                    Tile.drawTile(tiles[i][j], p);
+            }
+        }
+    }
+
+    @Override
+    public boolean isVisible() {
+        return this.visible;
+    }
+
+    @Override
+    public int compareTo(Drawable o) {
+        if (this.layerDepth < o.getLayerDepth())
+            return -1;
+        else if (this.layerDepth > o.getLayerDepth())
+            return 1;
+        return 0;
     }
 }
--- src/net/wintastic/wincremental/tiles/Tile.java
+++ src/net/wintastic/wincremental/tiles/Tile.java
@@ -1,65 +1,68 @@
 package net.wintastic.wincremental.tiles;
 
-import net.wintastic.lwjgl.DrawBatch;
-import net.wintastic.lwjgl.Drawable;
 import net.wintastic.lwjgl.Pair;
 import net.wintastic.lwjgl.Sprite;
+import net.wintastic.wincremental.AssetLibrary;
 import net.wintastic.wincremental.GameManager;
+import net.wintastic.wincremental.Player;
 import org.lwjgl.util.vector.Vector2f;
 
-public abstract class Tile implements Drawable {
-    Pair<Integer> position;
-    private float layerDepth;
+import java.math.BigInteger;
 
-    public Tile(Pair<Integer> position) {
-        this.position = position;
-        this.layerDepth = 0.2f;
-        DrawBatch.add(this);
+public abstract class Tile {
+    public enum TileType {
+        EMPTY(AssetLibrary.grassTileSprite, 0, 0),
+
+        TOWN_CENTER(AssetLibrary.townCenterSprite, 0, 16),
+        TENT(AssetLibrary.tentTileSprite, 0, 0),
+        STORAGE_SHED(AssetLibrary.storageShedTileSprite, 0, 0),
+
+        WOOD(AssetLibrary.woodTileSprite, 20, 0),
+        GOLD(AssetLibrary.goldTileSprite, 10, 0);
+
+        private final Sprite sprite;
+        private final int initialSize;
+        private int radiusSize;
+
+        TileType(Sprite sprite, int initialSize, int radiusSize) {
+            this.sprite = sprite;
+            this.initialSize = initialSize;
+            this.radiusSize = radiusSize;
+        }
+
+        public Sprite getSprite() {
+            return this.sprite;
+        }
+
+        public void clickAction() {
+            switch (this) {
+                case WOOD:
+                    GameManager.player.changeResource(Player.ResourceType.WOOD, BigInteger.ONE);
+                    break;
+                case GOLD:
+                    GameManager.player.changeResource(Player.ResourceType.GOLD, BigInteger.ONE);
+                    break;
+            }
+        }
     }
 
-    public abstract void clickAction();
+    public static void drawTile(TileType type, Pair<Integer> position) {
+        type.getSprite().position = getScreenPosition(position);
+        type.getSprite().draw();
+    }
 
-    protected abstract Sprite getSprite();
-
-    public Vector2f getScreenPosition() {
+    private static Vector2f getScreenPosition(Pair<Integer> position) {
         float x = position.first * GameManager.tileSize + GameManager.menuBarWidth - GameManager.camera.getPosition().x;
         float y = position.second * GameManager.tileSize + GameManager.toolbarHeight - GameManager.camera.getPosition().y;
         return new Vector2f(x, y);
     }
 
-    @Override
-    public void setLayerDepth(float layerDepth) {
-        this.layerDepth = layerDepth;
-    }
-
-    @Override
-    public float getLayerDepth() {
-        return this.layerDepth;
-    }
-
-    @Override
-    public void draw() {
-        getSprite().position = getScreenPosition();
-        getSprite().draw();
-    }
-
-    @Override
-    public boolean isVisible() {
+    public static boolean isVisible(Pair<Integer> position) {
         int minX = (int) (GameManager.camera.getPosition().x / GameManager.tileSize - 1);
         int maxX = (int) (GameManager.camera.getPosition().x / GameManager.tileSize + GameManager.viewportWidth + 1);
         int minY = (int) (GameManager.camera.getPosition().y / GameManager.tileSize - 1);
         int maxY = (int) (GameManager.camera.getPosition().y / GameManager.tileSize + GameManager.viewportHeight + 1);
 
         return position.first >= minX && position.first < maxX && position.second >= minY && position.second < maxY;
-    }
-
-    @Override
-    public int compareTo(Drawable o) {
-        if (this.layerDepth < o.getLayerDepth())
-            return -1;
-        else if (this.layerDepth > o.getLayerDepth())
-            return 1;
-        else
-            return 0;
     }
 }
