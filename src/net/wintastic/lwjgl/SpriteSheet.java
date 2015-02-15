443c4f009fa51e417cb007901251edc8606cacb8
Julien <julien.mageau@live.ca>
2015-02-14 23:19:30 -0500

    Added support for multiple lines to SpriteSheet and fixed SpriteSheet rotation

--- src/net/wintastic/lwjgl/SpriteSheet.java
+++ src/net/wintastic/lwjgl/SpriteSheet.java
@@ -6,21 +6,21 @@ public class SpriteSheet implements Drawable {
     public final Sprite sprite;
     private final int frameWidth;
     private final int frameHeight;
+    private final int framesCount;
     private final int frameDuration;
-    private float currentFrame;
-    private float frameCount;
+    private int currentFrame;
     private int frameTimer;
     private float layerDepth;
     private boolean visible;
 
 
-    public SpriteSheet(Sprite sprite, int frameWidth, int frameHeight, int frameDuration, float layerDepth) {
+    public SpriteSheet(Sprite sprite, int frameWidth, int frameHeight, int framesCount, int frameDuration, float layerDepth) {
         this.sprite = sprite;
         this.frameWidth = frameWidth;
         this.frameHeight = frameHeight;
+        this.framesCount = framesCount;
         this.frameDuration = frameDuration;
         this.currentFrame = 0;
-        this.frameCount = sprite.getWidth() / frameWidth;
         this.frameTimer = 0;
         this.layerDepth = layerDepth;
         this.visible = true;
@@ -34,7 +34,7 @@ public class SpriteSheet implements Drawable {
 
     public void draw() {
         if (frameTimer > frameDuration) {
-            currentFrame = (currentFrame + 1) % frameCount;
+            currentFrame = (currentFrame + 1) % framesCount;
             frameTimer = 0;
         }
         frameTimer++;
@@ -48,24 +48,28 @@ public class SpriteSheet implements Drawable {
 
         GL11.glPushMatrix();
 
-        GL11.glTranslatef(sprite.position.x + frameWidth * (currentFrame + 1f / 2), sprite.position.y + frameHeight / 2, 0);
+        GL11.glTranslatef(sprite.position.x + frameWidth / 2f, sprite.position.y + frameHeight / 2f, 0);
         GL11.glRotatef((float) Math.toDegrees(sprite.rotation), 0f, 0f, 1f);
-        GL11.glTranslatef(-sprite.position.x - frameWidth * (currentFrame + 1f / 2), -sprite.position.y - frameHeight / 2, 0);
+        GL11.glTranslatef(-sprite.position.x - frameWidth / 2f, -sprite.position.y - frameHeight / 2f, 0);
 
         GL11.glBegin(GL11.GL_QUADS);
 
-        float f = currentFrame / frameCount;
-        float f2 = (currentFrame + 1) / frameCount;
-        GL11.glTexCoord2f(sprite.getWidth() * f / Sprite.nextPowerOf2(sprite.getWidth()), 0);
+        float fH = (currentFrame % ((int) sprite.getWidth() / frameWidth)) / (sprite.getWidth() / frameWidth);
+        float fH2 = fH + 1 / (sprite.getWidth() / frameWidth);
+        float fV = (currentFrame / ((int) sprite.getWidth() / frameHeight)) / (sprite.getHeight() / frameHeight);
+        float fV2 = fV + 1 / (sprite.getHeight() / frameHeight);
+        System.out.println(fV);
+
+        GL11.glTexCoord2f(sprite.getWidth() * fH / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() * fV / Sprite.nextPowerOf2(sprite.getHeight()));
         GL11.glVertex2f(sprite.position.x, sprite.position.y);
 
-        GL11.glTexCoord2f(sprite.getWidth() * f2 / Sprite.nextPowerOf2(sprite.getWidth()), 0);
+        GL11.glTexCoord2f(sprite.getWidth() * fH2 / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() * fV / Sprite.nextPowerOf2(sprite.getHeight()));
         GL11.glVertex2f(sprite.position.x + frameWidth, sprite.position.y);
 
-        GL11.glTexCoord2f(sprite.getWidth() * f2 / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() / Sprite.nextPowerOf2(sprite.getHeight()));
+        GL11.glTexCoord2f(sprite.getWidth() * fH2 / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() * fV2 / Sprite.nextPowerOf2(sprite.getHeight()));
         GL11.glVertex2f(sprite.position.x + frameWidth, sprite.position.y + frameHeight);
 
-        GL11.glTexCoord2f(sprite.getWidth() * f / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() / Sprite.nextPowerOf2(sprite.getHeight()));
+        GL11.glTexCoord2f(sprite.getWidth() * fH / Sprite.nextPowerOf2(sprite.getWidth()), sprite.getHeight() * fV2 / Sprite.nextPowerOf2(sprite.getHeight()));
         GL11.glVertex2f(sprite.position.x, sprite.position.y + frameHeight);
 
         GL11.glEnd();
